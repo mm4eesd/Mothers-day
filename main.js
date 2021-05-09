f86b922c8bd0d76cd06416e6e0f1581f479b3575
mm4eesd <82920027+mm4eesd@users.noreply.github.com>
2021-05-09 08:54:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function one(){ 
+window.alert("Happy Mothers Day! I can't express my love to you. So I made this small website compared to your love and care for us. It would take me more then 7 lives to repay your favor to us. I love you so so so so so so so so so so so much!");
+}
